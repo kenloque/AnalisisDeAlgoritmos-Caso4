be9702381472a0f42c095114e699c0901b705e61
RolbinM <rolbinmendezb@gmail.com>
2021-03-19 20:36:19 -0600

    Finish

--- Code/main.cpp
+++ Code/main.cpp
@@ -18,72 +18,72 @@ int main(){
 void draw(double pWidth, double pHeight, List* pLines){
     Point* initialPoints[4];
     Point* nextPoints[4];
-    double inicioX = 0;
-    double inicioY = 0;
+    double inicioX = 0;															//--> 1
+    double inicioY = 0;															//--> 1						3 +
 
-    for(int i=0; i<10; i++){
-        int distX = pWidth-inicioX;
-		int distY = pHeight-inicioY;
-		int reduccionX = (distX*4)/100;
-		int reduccionY = (distY*4)/100;
-		int divRellenoX = (reduccionX*25)/100;
-		int divRellenoY = (reduccionY*25)/100;
+    for(int i=0; i<10; i++){													//--> 1 + (3...				19 +
+        int distX = pWidth-inicioX;												//--> 2
+		int distY = pHeight-inicioY;											//--> 2
+		int reduccionX = (distX*4)/100;											//--> 3
+		int reduccionY = (distY*4)/100;											//--> 3
+		int divRellenoX = (reduccionX*25)/100;									//--> 3
+		int divRellenoY = (reduccionY*25)/100;									//--> 3
 
-        createPointsDerechos(initialPoints, pWidth, pHeight, inicioX, inicioY);
-        for(int i=0;i<3;i++){
-            pLines->add(initialPoints[i],initialPoints[i+1]);
+        createPointsDerechos(initialPoints, pWidth, pHeight, inicioX, inicioY);	//--> 5						14 +
+        for(int i=0;i<3;i++){													//--> 4				N=3							
+            pLines->add(initialPoints[i],initialPoints[i+1]);					//--> 5
 			//Agrega el relleno de este cuadro
-			if(i%2 == 0){
-				(initialPoints[i+1])->y+=(divRellenoY);
-				pLines->add(initialPoints[i], initialPoints[i+1]);
-				(initialPoints[i+1])->y+=(divRellenoY);
-				pLines->add(initialPoints[i], initialPoints[i+1];
-				(initialPoints[i+1])->y+=(divRellenoY);
-				pLines->add(initialPoints[i], initialPoints[i+1]);
-				(initialPoints[i+1])->y+=(divRellenoY);
-				pLines->add(initialPoints[i], initialPoints[i+1]);
-				(initialPoints[i+1])->y-=(4*divRellenoY);
+			if(i%2 == 0){														//--> 1						50 +		
+				(initialPoints[i+1])->y+=(divRellenoY);							//--> 6
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->y+=(divRellenoY);							//--> 6
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->y+=(divRellenoY);							//--> 6
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->y+=(divRellenoY);							//--> 6
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->y-=(4*divRellenoY);						//--> 5
 			}else{
-				(initialPoints[i+1])->x+=(divRellenoX);
-				pLines->add(initialPoints[i], initialPoints[i+1]);
-				(initialPoints[i+1])->x+=(divRellenoX);
-				pLines->add(initialPoints[i], initialPoints[i+1]);
-				(initialPoints[i+1])->x+=(divRellenoX);
-				pLines->add(initialPoints[i], initialPoints[i+1]);
-				(initialPoints[i+1])->x+=(divRellenoX);
-				pLines->add(initialPoints[i], initialPoints[i+1]);
-				(initialPoints[i+1])->x-=(4*divRellenoX);
+				(initialPoints[i+1])->x+=(divRellenoX);							//--> 6						49 +
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->x+=(divRellenoX);							//--> 6
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->x+=(divRellenoX);							//--> 6
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->x+=(divRellenoX);							//--> 6
+				pLines->add(initialPoints[i], initialPoints[i+1]);				//--> 5
+				(initialPoints[i+1])->x-=(4*divRellenoX);						//--> 5
 			}
         }
-        pLines->add(initialPoints[4],initialPoints[1]);
-        (initialPoints[1])->x+=(divRellenoX);
-        pLines->add(initialPoints[4], initialPoints[1]);
-		(initialPoints[1])->x+=(divRellenoX);
-		pLines->add(initialPoints[4], initialPoints[1]);
-		(initialPoints[1])->x+=(divRellenoX);
-		pLines->add(initialPoints[4], initialPoints[1]);
-		(initialPoints[1])->x+=(divRellenoX);
-		pLines->add(initialPoints[4], initialPoints[1]);
-		(initialPoints[1])->x-=(4*divRellenoX);
+        pLines->add(initialPoints[4],initialPoints[1]);							//--> 4						41 +		
+        (initialPoints[1])->x+=(divRellenoX);									//--> 4
+        pLines->add(initialPoints[4], initialPoints[1]);						//--> 4
+		(initialPoints[1])->x+=(divRellenoX);									//--> 4
+		pLines->add(initialPoints[4], initialPoints[1]);						//--> 4
+		(initialPoints[1])->x+=(divRellenoX);									//--> 4
+		pLines->add(initialPoints[4], initialPoints[1]);						//--> 4
+		(initialPoints[1])->x+=(divRellenoX);									//--> 4
+		pLines->add(initialPoints[4], initialPoints[1]);						//--> 4
+		(initialPoints[1])->x-=(4*divRellenoX);									//--> 5
 
-        int auxInicioX = inicioX + reduccionX;
-		int auxInicioY = inicioY + reduccionY;
-		int auxAncho = pWidth - reduccionX;
-		int auxAlto = pHeight - reduccionY;
+        int auxInicioX = inicioX + reduccionX;									//--> 2						8 + 9 + 1 +
+		int auxInicioY = inicioY + reduccionY;									//--> 2
+		int auxAncho = pWidth - reduccionX;										//--> 2
+		int auxAlto = pHeight - reduccionY;										//--> 2
 
-        createPointsTorcidos(nextPoints, pWidth, pHeight, inicioX, inicioY, auxInicioX, auxInicioY, auxAncho, auxAlto);
-        for(int i=0;i<3;i++){
-            pLines->add(nextPoints[i],nextPoints[i+1]);
+        createPointsTorcidos(nextPoints, pWidth, pHeight, inicioX, inicioY, auxInicioX, auxInicioY, auxAncho, auxAlto);	//--> 9
+        for(int i=0;i<3;i++){													//--> 1+(3 +..				8 +
+            pLines->add(nextPoints[i],nextPoints[i+1]);							//--> 5)*N
 			//Agrega el relleno de este cuadro
 			          
         }
-        pLines->add(nextPoints[4],nextPoints[1]);
+        pLines->add(nextPoints[4],nextPoints[1]);								//--> 4						4 +
         
 
-        inicioX = auxInicioX;
-		inicioY = auxInicioY;
-	    pWidth = auxAncho;
-		pHeight = auxAlto;
+        inicioX = auxInicioX;													//--> 1						4 +
+		inicioY = auxInicioY;													//--> 1
+	    pWidth = auxAncho;														//--> 1
+		pHeight = auxAlto;														//--> 1
     }
 }
 
--- README.md
+++ README.md
@@ -7,8 +7,13 @@ Tarea universitaria de Análisis de Algoritmos
 El algoritmo funciona sobre un ciclo en el cual se crean y se guardan las coordenadas de cada linea de la imagen representada por un par de puntos. Los puntos son creados desde el valor de la altura y el ancho máximo y las coordenadas iniciales, los cuales son alterados de manera porcentual para reducir la imagen.
 
 ## F(n)
+Donde n=3 por el for interno...
 
+El f(n) = 3 + (19 + 14 + 50 + 49 + 41 + 18 + 8 + 4 + 4)*((3n+1)*n*)
 
+   f(n) = 3 + 207(3n^2 + n)
+   f(n) = 621n^2 + 207n + 3
 
 ## O(n)
-
+ 
+   O(n^2)
